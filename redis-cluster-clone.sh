f170817ad25f8c7492aaab54a02253abcb688899
xiaodong137038 <xiaodong137038@163.com>
2020-08-11 20:07:40 +0800

    update redis-cluster-clone.sh

--- redis-cluster-clone.sh
+++ redis-cluster-clone.sh
@@ -13,16 +13,22 @@ git version
 
 # clone docker-compose script files
 echo 'clone docker-compose script files'
-git init redis-tmp && cd redis-tmp
+#git init redis-tmp && cd redis-tmp
+git init
 git config core.sparsecheckout true
 echo 'redis-cluster' >> .git/info/sparse-checkout
 git remote add origin https://github.com/angleszhaixd/docker-compose.git
 git pull origin master
 
+chmod +x -R ./redis-cluster
+rm -rf ./.git
+cd ./redis-cluster
+
 # move docker-compose script files
-echo 'move docker-compose script files'
-mkdir -pv ../redis-cluster && cp -rf ./redis-cluster/* ../redis-cluster/
-cp -rf ./redis-cluster/.* ../redis-cluster/
-cd ../redis-cluster
-rm -rf ../redis-tmp
+#echo 'move docker-compose script files'
+#mkdir -pv ../redis-cluster && mv -f ./redis-cluster/* ../redis-cluster
+#cp -rf ./redis-cluster/.* ../redis-cluster
+#sleep 1
+#cd ../redis-cluster
+#rm -rf ../redis-tmp
 #alias proj="cd ./redis-cluster"
